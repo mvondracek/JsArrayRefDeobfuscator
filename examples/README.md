3f16797b4a8d2e0eae003c9a2b859e6d2171951c
Martin Vondráček <vondracek.mar@gmail.com>
2020-06-19 18:44:57 +0200

    docs: add example code

---
+++ examples/README.md
@@ -0,0 +1,1 @@
+For more short examples, please see implemented unit tests ([`/tests`](/tests)).
---
+++ examples/result.js
@@ -0,0 +1,14 @@
+var _0x01ff = ["foo",0,1,"bar"];
+function f() {
+  return "bar";
+}
+var a = "foo";
+a = [0 + 1,0 ? "foo" : 1];
+switch (f("foo")) {
+  case "foo":
+    a = "foo";
+    break;
+  default:
+    a = 1;
+    break;
+}
